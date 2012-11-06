61b7d5d75e8b000f9529bc3ff8450f5acc7dd16d
Jeremy Noetzelman <jeremy@pathable.com>
2012-11-05 23:36:38 -0800

    Bare Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aggregator::Application.config.secret_token = 'be3f04c08e165677151fbded4a939171d06a44b9fb14d3448a566ae097e42c29532fbf79e9b91d64a7dd928eead48b01e9771770dea5c4eb82ce60800ae1e906'
